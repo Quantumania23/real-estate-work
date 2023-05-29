c8c7bfb2bb6efc48fb422f6858099315631d49ea
quantumania23 <116637399+Quantumania23@users.noreply.github.com>
2023-05-29 20:20:51 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader (){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
