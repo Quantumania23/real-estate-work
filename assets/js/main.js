4844310d91ff8109a98c63bb299ffab1ce6476b8
quantumania23 <116637399+Quantumania23@users.noreply.github.com>
2023-05-30 19:32:18 +0300

    whats up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/* CHANGE BACKGROUND HEADER */
 function scrollHeader (){
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
@@ -7,19 +7,56 @@ function scrollHeader (){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SWIPER POPULAR ===============*/
+/* SWIPER POPULAR */
+let swiperPopular = new Swiper(".popular__container", {
+    spaceBetween:32,
+    grabCursor:true,
+    centredSlides:true,   
+    slidesPerView:'auto',
+    loop:true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+/* VALUE */
+const accordingItems = document.querySelectorAll('.value__according-item')
+
+accordingItems.forEach((item) => {
+    const accordingHeader =item.querySelector('.value__according-icon')
+
+    accordingHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.according-open')
+        toggleItem(item)
+
+        if (openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordingContent = item.querySelector('value__according-content')
+
+    if(item.classList.contains('according-open')){
+        accordingContent.removeAttribute('style')
+        item.classList.remove('according-open')
+    } else{
+        accordingContent.style.height = accordingContent.scrollHeight + 'px'
+        item.classList.add('according-open')
+    }
+
+}
+
+/* SCROLL SECTIONS ACTIVE LINK */
 
 
-/*=============== VALUE ACCORDION ===============*/
+/* SHOW SCROLL UP */ 
 
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/* DARK LIGHT THEME */ 
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/* SCROLL REVEAL ANIMATION */
